0cb6a7ef8da1f6f84dbb26065c2d0e35a672cdf5
eatdreamcat <meow.std@outlook.com>
2023-04-17 23:59:36 +0800

    ssssssss

--- Nova/winD3D/Helper.h
+++ Nova/winD3D/Helper.h
@@ -5,3 +5,25 @@
 // for the GPU lifetime of resources to avoid destroying objects that may still be
 // referenced by the GPU.
 using Microsoft::WRL::ComPtr;
+
+
+inline void GetAssetsPath(_Out_writes_(pathSize) WCHAR* path, UINT pathSize)
+{
+    if (path == nullptr)
+    {
+        throw std::exception();
+    }
+
+    DWORD size = GetModuleFileName(nullptr, path, pathSize);
+    if (size == 0 || size == pathSize)
+    {
+        // Method failed or path was truncated.
+        throw std::exception();
+    }
+
+    WCHAR* lastSlash = wcsrchr(path, L'\\');
+    if (lastSlash)
+    {
+        *(lastSlash + 1) = L'\0';
+    }
+}
--- Nova/winD3D/WindowBase.cpp
+++ Nova/winD3D/WindowBase.cpp
@@ -1,6 +1,28 @@
 #include"stdafx.h"
 #include "WindowBase.h"
+#include "Helper.h"
 
 using namespace Microsoft::WRL;
 
+WindowBase::WindowBase(UINT width, UINT height, std::wstring name) :
+	m_width(width),
+	m_height(height),
+	m_title(name)
+{
+	WCHAR assetsPath[512];
+	GetAssetsPath(assetsPath, _countof(assetsPath));
+	m_assetsPath = assetsPath;
 
+	m_aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+}
+
+WindowBase::~WindowBase()
+{
+
+}
+
+// Helper function for resolving the full path of assets.
+std::wstring WindowBase::GetAssetFullPath(LPCWSTR assetName)
+{
+	return m_assetsPath + assetName;
+}
